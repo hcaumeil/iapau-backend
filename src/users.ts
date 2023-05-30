2df4452222c9f48beaf1724c7c91ede3913610f8
MatisToniutti <47952310+MatisToniutti@users.noreply.github.com>
2023-05-30 12:38:39 +0200

    add delete and modify user routes

--- src/users.ts
+++ src/users.ts
@@ -13,6 +13,8 @@ class UsersController implements Controller {
     this.router.get(UsersController.path, this.get);
     this.router.post(UsersController.path,this.post);
     this.router.get(UsersController.path+"/:id",this.get_user);
+    this.router.delete(UsersController.path+"/:id",this.delete);
+    this.router.post(UsersController.path+"/:id",this.modify);
   }
 
   async get(req: Request, res: Response) {
@@ -76,11 +78,51 @@ class UsersController implements Controller {
       res.status(500).send("Informations not valid")
     }
   }
+
   async get_user(req: Request, res: Response) {
     try {  
       const result = await pg_client.query("SELECT * FROM users where id='"+req.params.id+"';");
       const users = result.rows;
       res.status(200).send(JSON.stringify(users));
+    } catch (error) {
+      res.status(500).send();
+    }
+  }
+
+  async delete(req: Request, res: Response) {
+    try {  
+      const result = await pg_client.query("DELETE FROM users where id='"+req.params.id+"';");
+      if(result.rowCount>0){
+        res.status(200).send(JSON.stringify("User deleted"));
+      }else{
+        res.status(400).send("Wrong id");
+      }
+      } catch (error) {
+        res.status(500).send();
+      }
+  }
+
+  async modify(req: Request, res: Response) {
+    try {      
+      const data = await req.body;
+      let query = "UPDATE users Set ";
+      if(!data){
+        res.status(400).send("No attributes to modify");
+      }else{
+        Object.entries(data).forEach(([key, value]) => {
+          query = query.concat(key+"='"+value+"',");
+          console.log(key + " , value : "+value);
+        });
+        query = query.slice(0, -1);
+        query = query.concat("WHERE id = '"+req.params.id+"';" );
+        console.log(query)
+        const result = await pg_client.query(query);
+        if(result.rowCount>0){
+          res.status(200).send(JSON.stringify("User modified"));
+        }else{
+          res.status(400).send("Informations not valid");
+        }
+      }
     } catch (error) {
       res.status(500).send();
     }
