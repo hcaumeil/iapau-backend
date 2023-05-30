9131841611ca4974b2eda172c8e9416c06957eca
MatisToniutti <47952310+MatisToniutti@users.noreply.github.com>
2023-05-30 13:00:15 +0200

    add team routes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import App from "./app.ts";
+import TeamController from "./team.js";
 import UsersController from "./users.js";
 
 const controllers = [
-  new UsersController()
+  new UsersController(),
+  new TeamController()
 ];
 
 const app = new App(
