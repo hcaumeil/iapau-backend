af630066f26963412d2ecab091f7b0296ee2d56d
MatisToniutti <47952310+MatisToniutti@users.noreply.github.com>
2023-05-30 13:40:07 +0200

    add data_challenge routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import App from "./app.ts";
 import TeamController from "./team.js";
+import DataChallengeController from "./data_challenge.js";
 import UsersController from "./users.js";
 
 const controllers = [
   new UsersController(),
-  new TeamController()
+  new TeamController(),
+  new DataChallengeController()
 ];
 
 const app = new App(
